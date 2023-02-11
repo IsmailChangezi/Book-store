0472dcf73165d9d129b90659c95a2fcab705caef
Ismail Ali <ismailchangezi801@gmail.com>
2023-02-11 12:31:21 +0500

    Fix Linter errors

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 // eslint-disable-next-line
 import { checkStatus } from "../redux/categories/categories";
 
