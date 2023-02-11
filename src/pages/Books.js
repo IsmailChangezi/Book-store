995514f817239f417339eac748959e4492c903c5
Ismail Ali <ismailchangezi801@gmail.com>
2023-02-11 12:29:39 +0500

    Fix Linter errors

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { useSelector } from 'react-redux';
 // eslint-disable-next-line
 import Book from "../components/Book";
 // eslint-disable-next-line
